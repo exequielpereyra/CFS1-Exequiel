4d28b9f347cc44e1d6f67770ba85ecfde080e3e7
exequielpereyra <exe1391@hotmail.com>
2024-02-29 21:25:54 -0300

    Mi primer commit

---
+++ Clase1/ejercicio1.ts
@@ -0,0 +1,11 @@
+console.log("Pasos para cocinar una torta:")
+console.log("1. Precalentar el horno");
+console.log("2. Enmantecar el molde");
+console.log("3. Mezclar la harina y los huevos en un bowl");
+console.log("4. Agregar azúcar y batir los ingredientes");
+console.log("5. Vertir la mezcla en el molde");
+console.log("6. Cocinar en el horno por 50 minutos");
+console.log("7. Retirar del horno");
+console.log("8. Corroborar que la torta está cocida por dentro");
+console.log("9. Cortarla en porciones");
+console.log("10. A comer!");
